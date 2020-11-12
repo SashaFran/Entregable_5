b4ec4cbd0f0fc6605ef7091785502e92c933c2b1
Sashafran <sashafranchini@gmail.com>
2020-11-11 23:54:07 -0300

    nuevo modulo/componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { NoLogginModule } from './components/noLoggin/no-loggin.module';
 
 import { AppComponent } from './app.component';
+import { NavBarLogComponent } from './components/loggeado/nav-bar-log/nav-bar-log.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarLogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/loggeado/loggeado.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterLogComponent } from './footer-log/footer-log.component';
+
+
+
+@NgModule({
+  declarations: [FooterLogComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoggeadoModule { }

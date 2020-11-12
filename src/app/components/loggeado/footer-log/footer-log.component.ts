49aa8101af7f2ec6274b6d60e2f1d75976c132d6
Sashafran <sashafranchini@gmail.com>
2020-11-12 15:25:01 -0300

    arreglo nav, colocacion barra repro

--- src/app/components/loggeado/footer-log/footer-log.component.ts
+++ src/app/components/loggeado/footer-log/footer-log.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer-log',
   templateUrl: './footer-log.component.html',
   styleUrls: ['./footer-log.component.css']
 })
-export class FooterLogComponent implements OnInit {
+export class FooterLogComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    console.log("Footer loggeado funciona");
   }
+
 
 }

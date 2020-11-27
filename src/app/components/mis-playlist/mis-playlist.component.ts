749e93390a17a164b23e5158f751ca42532c16ad
leandro Arista <leandroalbertoarista@gmail.com>
2020-11-27 13:34:02 -0300

    fix mis playlist add hover

--- src/app/components/mis-playlist/mis-playlist.component.ts
+++ src/app/components/mis-playlist/mis-playlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SongInfo } from 'src/app/song-info';
 
 @Component({
   selector: 'app-mis-playlist',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mis-playlist.component.css']
 })
 export class MisPlaylistComponent implements OnInit {
-
+  titulo:string[] = ['Lo más escuchado','Sugerencias para ti','Ultimos Agregados','Nuestas Playlist','Pasar el rato'];
+  array:SongInfo[] =[
+    new SongInfo('1','Ayer me llamo mi ex',"",'3:00',"artista","album","../../assets/img/songs/ayer.png"),
+    new SongInfo('2','Stay with me',"",'3:10',"artista","album","../../assets/img/songs/bird.png"),
+    new SongInfo('3','BlurryFace',"",'3:00',"artista","album","../../assets/img/songs/blurry.png"),
+    new SongInfo('4','Colores',"",'3:10',"artista","album","../../assets/img/songs/colores.png"),
+    new SongInfo('5','CryBaby',"",'3:00',"artista","album","../../assets/img/songs/cry.png"),
+    new SongInfo('6','That Bitch!',"",'3:10',"artista","album","../../assets/img/songs/thatbitch.png"),
+    new SongInfo('7','Thousand',"",'3:00',"artista","album","../../assets/img/songs/thousand.png"),
+    new SongInfo('8','Trench',"",'3:10',"artista","album","../../assets/img/songs/trench.png")
+  ];
+  lista:SongInfo[]=[
+    new SongInfo('9',"Correr","","80:00","","","../../assets/img/list/correr.png"),
+    new SongInfo('10',"Jugar","","80:00","","","../../assets/img/list/jugar.png"),
+    new SongInfo('11',"Bailar","","80:00","","","../../assets/img/list/bailar.png"),
+  ];
   constructor() {
     console.log("Mis PlayLists funciona")
    }
